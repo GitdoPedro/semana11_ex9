23c37820c501bf2525f31fe757fa26f0647be1d8
GitdoPedro <emailpedrolima@gmail.com>
2023-01-19 18:16:42 -0300

    [M1S11] Ex 8 - Revisão geral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'semana11_ex8';
+export class AppComponent implements OnChanges{
+  public taskList: Array<any> = []
+  public vazia: boolean = false
+  public tarefa: any = ''
+
+  ngOnChanges(){
+    if ((this.taskList).length>0){
+      
+    }
+    
+  }
+
+  adicionaTarefa(){
+    this.taskList.push(this.tarefa)
+    console.log(this.taskList)
+    this.vazia = true
+    console.log(this.vazia)
+  }
+
 }
